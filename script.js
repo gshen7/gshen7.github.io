406264460793d5dd3152fc1beb53357cf412b174
gshen7 <gshen7@uwo.ca>
2019-07-23 19:29:00 -0700

    animated loading icon, delay to redirect

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //redirect to notion page
 window.onload = function () {
-  window.location.replace("https://www.notion.so/gshen7/Gary-Shen-486a836e6add478f83d45ea92c7d1e2d");
+  setTimeout(function () {
+    window.location.replace("https://www.notion.so/gshen7/Gary-Shen-486a836e6add478f83d45ea92c7d1e2d");
+  }, 1000);
 };
